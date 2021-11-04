5b92f22ca9e8507dd63bb59d1c8e501dedbc8bc0
dharshatharan <dharshatharan@gmail.com>
2021-11-03 23:58:52 -0400

    Adding more article data

--- api/routes/articles.js
+++ api/routes/articles.js
@@ -1,15 +1,39 @@
 var express = require('express');
 var router = express.Router();
 var fg = require("fast-glob")
+var fs = require('fs');
+var path = require('path');
+var matter = require('gray-matter');
+var readTime = require('reading-time');
 
 /* GET articles. */
 router.get('/', function (req, res, next) {
   const files = fg.sync(["*.md", "*.mdx"], { cwd: "../articles" });
-  const urls = files.map(file => {
+  const filesData = files.map(file => {
     // return req.protocol + '://' + req.get('host') + req.originalUrl + file
-    return file;
+    // return file;
+    console.log(path.join(__dirname, '../../articles/' + file))
+    const fileContents = fs.readFileSync(path.join(__dirname, '../../articles/', file), "utf8");
+    const matterResult = matter(fileContents);
+
+    console.log(matterResult);
+    const data = matterResult.data;
+    return {
+      id: file.replace('.mdx', '').replace('.md', ''),
+      title: data.title,
+      file: file,
+      url: req.protocol + '://' + req.get('host') + req.originalUrl + file,
+      publisedAt: data.publisedAt,
+      updatedAt: data.updatedAt,
+      tags: data.tags,
+      readingTime: readTime(matterResult.content),
+      description: data.description,
+      author: data.author,
+      image: data.image,
+      // content: matterResult.content,
+    }
   });
-  res.send(urls);
+  res.send(filesData);
 });
 
 module.exports = router;
